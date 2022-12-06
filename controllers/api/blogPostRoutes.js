498614d818e4cba27067525816d7a0627f9bee8a
Blake Burns <bburns@mysgi.com>
2022-12-06 16:10:52 -0500

    updated query selector ids in login.js, added comment routes to blogPostRoutes.js

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { response } = require('express');
 const { request } = require('http');
-const { BlogPost } = require('../../models');
+const { BlogPost, Comment } = require('../../models');
 const authorized = require('../../utils/authorization');
 
 router.post('/', authorized, async (request, response) => {
@@ -49,6 +49,56 @@ router.delete('/:id', authorized, async (request, response) => {
         }
 
         response.status(200).json(blogPostData);
+    } catch (error) {
+        response.status(500).json(error);
+    }
+});
+
+router.post('/', authorized, async (request, response) => {
+    try {
+        const newComment = await Comment.create({
+            ...request.body,
+            user_id: request.statusMessage.user_id,
+        });
+
+        response.status(200).json(newComment);
+    } catch (error) {
+        response.status(400).json(error);
+    }
+});
+
+router.put('/:id', authorized, async (request, response) => {
+    try {
+        const commentData = await Comment.update(request.body, {
+            where: {
+                id: request.params.id,
+            },
+        });
+        if (!commentData[0]) {
+            response.status(404).json({ message: "Comment not found..." });
+            return;
+        }
+        response.status(200).json(commentData);
+    } catch (error) {
+        response.status(500).json(error);
+    }
+});
+
+router.delete('/:id', authorized, async (request, response) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: request.params.id,
+                user_id: request.session.user_id,
+            },
+        });
+
+        if (!commentData) {
+            response.status(404).json({ message: "Comment not found..."});
+            return;
+        }
+
+        response.status(200).json(commentData);
     } catch (error) {
         response.status(500).json(error);
     }
--- public/js/login.js
+++ public/js/login.js
@@ -1,19 +1,19 @@
 const loginIn = async (event) => {
     event.preventDefault();
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const userName = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
 
-    if (email && password) {
+    if (userName && password) {
         
         const response = await fetch('/api.users/login', {
             method: 'POST',
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({ userName, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
     if (response.ok) {
-        document.location.replace('/profile');
+        document.location.replace('/dashboard');
     } else {
         alert(response.statusText);
     }
@@ -23,9 +23,9 @@ const loginIn = async (event) => {
 const signUp = async (event) => {
     event.preventDefault();
 
-    const userName = document.querySelector('#user-name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const userName = document.querySelector('#new-username').value.trim();
+    const email = document.querySelector('#new-email').value.trim();
+    const password = document.querySelector('#new-password').value.trim();
 
     if (userName && email && password) {
         const response = await fetch('/api/users', {
@@ -35,7 +35,7 @@ const signUp = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace('/profile');
+            document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
         }
