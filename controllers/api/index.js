525876e1ba47e70dcca0e35769910d67f644bdcc
Blake Burns <bburns@mysgi.com>
2022-12-20 18:16:59 -0500

    renamed blogpost route file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const blogpostRoutes = require('./blogpostRoutes');
+const blogpostRoutes = require('./blogPostRoutes');
 const commentRoutes = require('./commentRoutes')
 
 router.use('/user', userRoutes);
